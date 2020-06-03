9b55f7e6e7aebc4470d5be332adbaaa696b3086d
Bartosz Konarzewski <s15198@pjwstk.edu.pl>
2020-06-03 14:54:14 +0200

    added model classes

---
+++ DOT.NET/Models/Kategoria.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DOT.NET.Models
+{
+    public class Kategoria
+    {
+        public int KategoriaId { get; set; }
+        public string NazwaKategorii { get; set; }
+        public string OpisKategorii { get; set; }
+        public string NazwaPlikuIkony { get; set; }
+        public virtual ICollection<Kurs> Kursy { get; set; }
+    }
+}
---
+++ DOT.NET/Models/PozycjaZmowienia.cs
@@ -0,0 +1,14 @@
+﻿namespace DOT.NET.Models
+{
+    internal class PozycjaZmowienia
+    {
+        public int PozycjaZamowieniaId { get; set; }
+        public int ZamowienieId { get; set; }
+        public int KursId { get; set; }
+        public int Ilosc { get; set; }
+        public decimal CenaZakupu { get; set; }
+    
+        public virtual Kurs kurs { get; set; }
+        public virtual Zamowienie zamowienie { get; set; }
+    }
+}
